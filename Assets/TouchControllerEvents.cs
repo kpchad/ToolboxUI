285648eccf8e47b14e756532de25d7a0ef1f5d85
kpchad <kylepchad@gmail.com>
2019-11-10 20:56:46 -0800

    before brininging in VRTK

---
+++ Assets/TouchControllerEvents.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OVRTouchSample
+{
+    public class TouchControllerEvents : MonoBehaviour
+    {
+
+        public GameObject toolbox;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // if left controller "A" is pressed
+            if (OVRInput.Get(OVRInput.Button.Four) == true) {
+                if (toolbox.activeSelf == false)
+                {
+                    //show tool assets
+                    toolbox.SetActive(true);
+                }
+                else
+                {
+                    //hide tool assets
+                    toolbox.SetActive(false);
+                }
+                
+            }
+
+            
+        }
+    }
+}
